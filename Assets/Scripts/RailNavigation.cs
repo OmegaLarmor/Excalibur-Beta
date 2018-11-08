aeb8293fb8e23a745b2ba27bbc4270d6f9774860
RayTeX <remy-lapointe@hotmail.ca>
2018-11-08 09:30:21 -0500

    First commit

---
+++ Assets/Scripts/RailNavigation.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class RailNavigation : MonoBehaviour {
+
+	public Transform railBegin;
+	public Transform railEnd;
+
+	private NavMeshAgent agent;
+
+	// Use this for initialization
+	void Start () {
+		
+		agent = GetComponent<NavMeshAgent>();
+
+		agent.SetDestination(railEnd.position);
+
+		Debug.Log(railEnd.position);
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
